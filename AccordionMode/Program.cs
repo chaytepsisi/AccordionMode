52fda0e38bb7fe357bf22de501093097d4c14682
onur.kocak <onur.kocak@is>
2025-02-24 14:17:16 +0300

    * Main now includes a enc-dec check.

--- AccordionMode/Program.cs
+++ AccordionMode/Program.cs
@@ -21,9 +21,25 @@ namespace AccordionMode
             for (int i = 0; i < tag.Length; i++)
                 tag[i] = (byte)i;
 
-            var output=threeBranchAlg.Encipher(pText, key, tag);
+            var output = threeBranchAlg.Encipher(pText, key, tag);
+            //Console.WriteLine("Ptext: "+ Commons.ByteArrayToString(pText));
+            //Console.WriteLine("Key: " + Commons.ByteArrayToString(key));
+            //Console.WriteLine("Tag: " + Commons.ByteArrayToString(tag));
 
             Console.WriteLine(Commons.ByteArrayToString(output));
+
+            var input = threeBranchAlg.Decipher(output, key, tag);
+
+            Console.WriteLine(Commons.ByteArrayToString(input));
+
+            bool equal = true;
+            for (int i = 0; i < input.Length; i++)
+                if (input[i] != pText[i])
+                    equal = false;
+
+            if (equal)
+                Console.WriteLine("Success!!");
+            else Console.WriteLine("ERROR!!");
         }
     }
 }
